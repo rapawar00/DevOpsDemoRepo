d73c72b58fa5f257de78cffacb4e7f83ffe30046
Rahul Pawar <76149780+rapawar00@users.noreply.github.com>
2026-04-25 19:56:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("Hello, Jenkins!\n This is Automated once developer pull the code on repo");
     }
 }
